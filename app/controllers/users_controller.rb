426b1464f1a9b9a508525a98c7916baacf6cabb7
Arnaud Lenglet <arlenglet@gmail.com>
2016-06-15 20:50:56 +0200

    feat(controller): finish UsersController#index #show implementation + specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = UserDecorator.decorate_collection(User.all)
   end
 
   def show
+    @user = User.find_by(username: params[:username])
+    @repositories = @user.repositories
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { create(:user) }
 
   describe "GET #index" do
     before { get :index }
-    it { expect(response).to have_http_status(:success) }
+    it do
+      expect(response).to have_http_status(:success)
+      expect(assigns(:users)).to eq([user])
+      expect(assigns(:users).first).to be_decorated_with(UserDecorator)
+    end
   end
 
   describe "GET #show" do
-    let(:user) { create(:user) }
-    before { get :show, username: user.username }
-    it { expect(response).to have_http_status(:success) }
+    let(:repository) { create(:repository) }
+
+    before do
+      user.repositories << repository
+      get :show, params: { username: user.username }
+    end
+
+    it do
+      expect(response).to have_http_status(:success)
+      expect(assigns(:user)).to eq(user)
+      expect(assigns(:repositories)).to eq([repository])
+    end
   end
 
 end
