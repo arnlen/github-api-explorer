565f7baf8d4e0dcc75adc0e876afaaa26868aafc
Arnaud Lenglet <arlenglet@gmail.com>
2016-06-16 00:02:56 +0200

    feat(service): implement GithubSyncService#synchronize_user_with + specs

--- app/services/github_sync_service.rb
+++ app/services/github_sync_service.rb
@@ -1,10 +1,60 @@
 class GithubSyncService
 
-  def initialize(username)
-    @username = username
+  def initialize(user)
+    @user = user
   end
 
-  def synchronize_user
+  def synchronize_user_with(resource)
+    @user.update_attribute(:followers, resource.followers) if @user.followers != resource.followers
+    local_repositories = @user.repositories
+    github_repositories = get_repos_for(resource)
+
+    local_repositories.each do |repository|
+      delete_repository_if_gone(repository, github_repositories)
+      update_repository_if_needed(repository, github_repositories)
+    end
+
+    create_missing_repositories(local_repositories, github_repositories)
+
+    @user
+  end
+
+  private
+
+  # DUPLICATION (Arnaud Lenglet): duplicated in GithubRetrieveService
+  def get_repos_for(user_resource)
+    user_resource.rels[:repos].get.data
+  end
+
+  def create_repository_from_attributes(repo_attributes)
+    Repository.create(name: repo_attributes[:name],
+                      creation_date: Date.parse(repo_attributes[:created_at]),
+                      stars: repo_attributes[:stargazers_count],
+                      user_id: @user.id)
+  end
+
+  def delete_repository_if_gone(repository, github_repositories)
+    unless github_repositories.any? { |repo_attributes| repo_attributes[:name] == repository.name }
+      repository.delete
+    end
+  end
+
+  def update_repository_if_needed(repository, github_repositories)
+    github_repositories.each do |repo_attributes|
+      if repo_attributes[:name] == repository.name
+        unless repo_attributes[:stargazers_count] == repository.stars
+          repository.update_attribute(:stars, repo_attributes[:stargazers_count])
+        end
+      end
+    end
+  end
+
+  def create_missing_repositories(local_repositories, github_repositories)
+    github_repositories.each do |repo_attributes|
+      unless local_repositories.pluck(:name).include?(repo_attributes[:name])
+        create_repository_from_attributes(repo_attributes)
+      end
+    end
   end
 
 end
--- spec/services/github_sync_service_spec.rb
+++ spec/services/github_sync_service_spec.rb
@@ -1,8 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe GithubSyncService do
-  subject { described_class.new(username) }
+  let(:user) { build_stubbed(:user) }
+  subject { described_class.new(user).synchronize_user_with(resource) }
 
-  describe "#retrieve_user" do
+  describe "#synchronize_user_with" do
+    let(:resource) { double login: user.username, followers: user.followers }
+    let(:repository_1) { build_stubbed(:repository) }
+    let(:repository_2) { build_stubbed(:repository) }
+    let(:relation) {{ repos: double }}
+    let(:repos_array) {[
+      { name: repository_1.name, stargazers_count: repository_1.stars, created_at: '2012-08-09 09:45:59 UTC' },
+      { name: repository_2.name, stargazers_count: repository_2.stars, created_at: '2012-08-09 09:45:59 UTC' }
+    ]}
+
+    before do
+      allow(user).to receive(:update_attribute)
+      allow(user).to receive(:repositories).and_return([repository_1, repository_2])
+      allow(repository_1).to receive(:update_attribute)
+      allow(repository_2).to receive(:update_attribute)
+      allow(repository_2).to receive(:delete)
+      allow(resource).to receive(:rels).and_return(relation)
+      allow(relation[:repos]).to receive_message_chain(:get, :data).and_return(repos_array)
+    end
+
+    context "when user's data are outdated" do
+      let(:resource) { double login: user.username, followers: 60 }
+      before { @result = subject }
+      it { expect(user).to have_received(:update_attribute) }
+      it { expect(@result).to eq(user) }
+    end
+
+    context "when user's data are up to date" do
+      before { subject }
+      it { expect(user).not_to have_received(:update_attribute) }
+    end
+
+    context "when an user's repository had been deleted on GitHub" do
+      let(:repos_array) {[{ name: repository_1.name, stargazers_count: repository_1.stars, created_at: '2012-08-09 09:45:59 UTC' }]}
+      before { subject }
+      it { expect(repository_2).to have_received(:delete) }
+    end
+
+    context "when user's repositories' data are outdated" do
+      let(:repos_array) {[
+        { name: repository_1.name, stargazers_count: 1, created_at: '2012-08-09 09:45:59 UTC' },
+        { name: repository_2.name, stargazers_count: 2, created_at: '2012-08-09 09:45:59 UTC' }
+      ]}
+      before { subject }
+      it { expect(repository_1).to have_received(:update_attribute) }
+      it { expect(repository_2).to have_received(:update_attribute) }
+    end
+
+    context "when user's repositories' data are up to date" do
+      before { subject }
+      it { expect(repository_1).not_to have_received(:update_attribute) }
+      it { expect(repository_2).not_to have_received(:update_attribute) }
+    end
   end
 end
